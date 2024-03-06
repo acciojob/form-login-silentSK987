a6e4c33188e6d98ea995ad880ddb82267f344736
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2024-03-06 16:53:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// 1st method
 function getFormvalue() {
     //Write your code here
+    const names = document.querySelectorAll("input");
+    alert(names[0].value + " " + names[1].value);
 
 }
+
+// ------------2nd method ---------
+// function getFormvalue() {
+//     var x=document.getElementById("form1");
+//     let name = ""
+//     for (var i=0;i<x.length;i++) {
+//         if (x.elements[i].value!='Submit')
+//             name += x.elements[i].value + " "
+//     }
+//     alert(name.substring(0,name.length-1));
+// }
+// console.log(`${document.querySelector("input[0].value")}` )
